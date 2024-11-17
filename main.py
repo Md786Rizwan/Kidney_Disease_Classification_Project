f526fbccc11f2adc783576aafad3d1c414274a09
Md Rizwan <imdrizwan2019@gmail.com>
2024-11-17 12:25:08 +0530

    added

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 from src.cnnClassifier import logger
-from src.cnnClassifier.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline # type: ignore
-from src.cnnClassifier.config.configuration import ConfigurationManager
+from src.cnnClassifier.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline 
 
 STAGE_NAME = "Data Ingestion stage"
 try:
